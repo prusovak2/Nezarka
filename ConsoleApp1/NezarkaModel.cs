98bd81c24f84b74af7c7db702cd0beac90994094
prusovak2 <prusova.k2@seznam.cz>
2019-11-04 18:02:40 +0100

    input check dione, not tested

--- ConsoleApp1/NezarkaModel.cs
+++ ConsoleApp1/NezarkaModel.cs
@@ -58,33 +58,63 @@ namespace NezarkaBookstore
                     switch (tokens[0])
                     {
                         case "BOOK":
-                            store.books.Add(new Book
-                            {                               
-                                Id = Int32.TryParse(tokens[1]),
-                                Title = tokens[2],
-                                Author = tokens[3],
-                                Price = decimal.Parse(tokens[4])
-                            });
-                            break;
-                        case "CUSTOMER":
-                            store.customers.Add(new Customer
+                            if (tokens.Length != 5)
                             {
-                                Id = int.Parse(tokens[1]),
-                                FirstName = tokens[2],
-                                LastName = tokens[3]
-                            });
-                            break;
-                        case "CART-ITEM":
-                            var customer = store.GetCustomer(int.Parse(tokens[1]));
-                            if (customer == null)
-                            {
+                                Console.WriteLine("Data error.");
                                 return null;
                             }
-                            customer.ShoppingCart.Items.Add(new ShoppingCartItem
+                            if (Book.TryParse(tokens[1],tokens[2], tokens[3], tokens[4], out Book b))
                             {
-                                BookId = int.Parse(tokens[2]),
-                                Count = int.Parse(tokens[3])
-                            });
+                                store.books.Add(b);
+                            }
+                            else //parse failed, number was expected
+                            { 
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            break;
+                        case "CUSTOMER":
+                            if (tokens.Length != 4)
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            if (Customer.TryParse(tokens[1], tokens[2], tokens[3], out Customer c))
+                            {
+                                store.customers.Add(c);
+                            }
+                            else //parse failed, number was expected
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            break;
+                        case "CART-ITEM":
+                            if (tokens.Length != 4)
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            if(!Int32.TryParse(tokens[1], out int custID))
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            var customer = store.GetCustomer(custID);
+                            if (customer == null)
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
+                            if(ShoppingCartItem.TryParse(tokens[2], tokens[3], out ShoppingCartItem s))
+                            {
+                                customer.ShoppingCart.Items.Add(s);
+                            }
+                            else //parse failed, number was expected
+                            {
+                                Console.WriteLine("Data error.");
+                                return null;
+                            }
                             break;
                         default:
                             Console.WriteLine("Data error.");
@@ -105,12 +135,34 @@ namespace NezarkaBookstore
         }
     }
 
-    class Book
+     class Book
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public decimal Price { get; set; }
+
+        public static bool TryParse(string id1, string title2, string author3, string price4, out Book b)
+        {
+            if(!Int32.TryParse(id1, out int id))
+            {
+                b = null;
+                return false;
+            }
+            if(!decimal.TryParse(price4, out decimal price))
+            {
+                b = null;
+                return false;
+            }
+            b = new Book
+            {
+                Id = id,
+                Title = title2,
+                Author = author3,
+                Price = price,
+            };
+            return true;
+        }
 
     }
 
@@ -137,12 +189,47 @@ namespace NezarkaBookstore
                 shoppingCart = value;
             }
         }
+        public static bool TryParse(string id1, string name2, string surname3, out Customer c)
+        {
+            if (!Int32.TryParse(id1, out int id))
+            {
+                c = null;
+                return false;
+            }
+            c = new Customer
+            {
+                Id = id,
+                FirstName = name2,
+                LastName = surname3
+            };
+            return true;
+        }
     }
 
     class ShoppingCartItem
     {
         public int BookId { get; set; }
         public int Count { get; set; }
+
+        public static bool TryParse(string bookId1, string count2, out ShoppingCartItem s)
+        {
+            if (!Int32.TryParse(bookId1, out int bookId))
+            {
+                s = null;
+                return false;
+            }
+            if (!Int32.TryParse(count2, out int count))
+            {
+                s = null;
+                return false;
+            }
+            s = new ShoppingCartItem
+            {
+                BookId = bookId,
+                Count = count,
+            };
+            return true;
+        }
     }
 
     class ShoppingCart
