bccdcdc511bf6d305fcae1a1dcb7ffb78de9290b
prusovak2 <prusova.k2@seznam.cz>
2019-11-04 16:37:24 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("NezarkaTests")]
+namespace NezarkaBookstore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NezarkaTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NezarkaBookstore;
+
+
+namespace NezarkaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
